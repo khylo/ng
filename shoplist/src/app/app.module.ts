6d9524d652c242f84a2fbd1f988bd9089c2b18ae
Keith <keith_hyland@yahoo.com>
2017-11-05 23:15:27 +0000

    working before refator

--- shoplist/src/app/app.module.ts
+++ shoplist/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoplistComponent } from './shoplist/shoplist.component';
+import { ShoplistEditComponent } from './shoplist-edit/shoplist-edit.component';
+import { IngredientComponent } from './ingredient/ingredient.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoplistComponent,
+    ShoplistEditComponent,
+    IngredientComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
